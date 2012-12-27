d60c80b38035e6b35ff7ce5a6d1e657ca6352c72
StephanB007@gmail.com <StephanB007@gmail.com@6c8a7419-725d-e037-4fe3-c1c9dc6cb3a7>
2012-12-27 01:19:06 +0000

    Change name of sqlite4m.zip to sqlite4m_source.zip

--- trunk/sql_close.m
+++
@@ -1,7 +0,0 @@
-function sql_close(dbp)
-% SQL_CLOSE - closes an SQLITE database connection previously openend with sql_open.
-%   See also http://www.sqlite.org/c3ref/close.html
-%
-%   Input:
-%      dbp         Pointer (handle) to the database connection structure previously returned
-%                  by sql_open.
